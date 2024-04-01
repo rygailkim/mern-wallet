e03cb3711a6aa4636965056cacb7dd63fa698b8a
unknown <rygailkim@gmail.com>
2024-04-01 13:12:35 +0800

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button } from 'antd';
+import './stylesheets/text-elements.css'
+import './stylesheets/form-elements.css'
+import './stylesheets/custom-components.css'
+import './stylesheets/alignments.css'
+import './stylesheets/theme.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Sheywallet Dev</h1>
+      <Button type="primary">Button</Button>
+    </div>
+  )
+}
+
+export default App
